56d7160b170bba431faaf65253807b910fa8cdcf
Alex Maass <am838@cornell.edu>
2014-09-30 10:47:49 -0400

    Added popups for naming the regions of ethiopia.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,14 +1,21 @@
 /**
 * Maps Javascript
 */
+var infoWindow;
+
+function infoWindowContent(name, description) {
+   content =   '<div class="FT_infowindow"><h3>' + name + 
+               '</h3><div>' + description + '</div></div>';
+   return content;
+}
 
 //Initialization
 var myLatlng = new google.maps.LatLng(9.195761, 40.498867);
 var mapOptions = {
   zoom: 6,
   center: myLatlng,
-  // mapTypeId: google.maps.MapTypeId.SATELLITE
-  mapTypeId: google.maps.MapTypeId.HYBRID
+  mapTypeId: google.maps.MapTypeId.HYBRID,
+  scaleControl: true
 
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -16,13 +23,20 @@ var map = new google.maps.Map(document.getElementById("map-canvas"),
 map.setTilt(0);
 
 //Country overlay
-var world_geometry = new google.maps.FusionTablesLayer({
+var layer = new google.maps.FusionTablesLayer({
   query: {
     select: 'kml_4326', 
     from: '420419', 
     where: "'name_0' = 'Ethiopia'"
   },
   map: map,
-  suppressInfoWindows: false
+  suppressInfoWindows: true
 });
 
+infoWindow = new google.maps.InfoWindow();
+google.maps.event.addListener(layer, "click", function(event) {
+  infoWindow.close();
+  infoWindow.setContent(infoWindowContent(event.row.name_1.value,event.row.name_0.value));
+  infoWindow.setPosition(event.latLng);
+  infoWindow.open(map);
+});
