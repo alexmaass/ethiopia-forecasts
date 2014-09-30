2b135a11f68ae67f5ec076d71e6e2c3a71916d35
Alex Maass <am838@cornell.edu>
2014-09-30 02:06:33 -0400

    Made google maps appear along with the outline of states in Ethiopia.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,28 @@
 /**
 * Maps Javascript
 */
-L.mapbox.accessToken = 'pk.eyJ1IjoiYW04MzgiLCJhIjoiMmdjdlpKUSJ9.DIBX2rAhuqs3WvQ-rrbHCg';
-var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png', {
-  attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+
+//Initialization
+var myLatlng = new google.maps.LatLng(9.195761, 40.498867);
+var mapOptions = {
+  zoom: 6,
+  center: myLatlng,
+  // mapTypeId: google.maps.MapTypeId.SATELLITE
+  mapTypeId: google.maps.MapTypeId.HYBRID
+
+};
+var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+map.setTilt(0);
+
+//Country overlay
+var world_geometry = new google.maps.FusionTablesLayer({
+  query: {
+    select: 'kml_4326', 
+    from: '420419', 
+    where: "'name_0' = 'Ethiopia'"
+  },
+  map: map,
+  suppressInfoWindows: false
 });
 
-var lat = 39.647469;
-var lng = -101.250000;
-var map = L.map('map').addLayer(mapboxTiles).setView([lat, lng], 4);
--- app/views.py
+++ app/views.py
@@ -2,5 +2,6 @@ from flask import *
 from app import app
 
 @app.route('/')
+@app.route('/index.html')
 def main():
   return render_template('index.html')
