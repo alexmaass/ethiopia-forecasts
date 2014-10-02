1e914a5afa5865ece0c02bcc8fdd9d719e3cc0d8
Alex Maass <am838@cornell.edu>
2014-10-01 21:25:27 -0400

    Refactored the project structure.

--- app/views.py
+++
@@ -1,7 +0,0 @@
-from flask import *
-from app import app
-
-@app.route('/')
-@app.route('/index.html')
-def main():
-  return render_template('index.html')
--- application.py
+++ application.py
@@ -1,3 +1,11 @@
 #!flask/bin/python
-from app import app
-app.run(debug=True)
+from flask import Flask, render_template
+application = Flask(__name__)
+
+@application.route('/')
+@application.route('/index.html')
+def main():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+    application.run(debug=True)
