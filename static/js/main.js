044390c31d2adc29e1a8df645aef5ed4628fe3cd
Alex Maass <am838@cornell.edu>
2014-10-29 22:33:07 -0400

    Massive maps update
    Finished plotting all waredas on google maps. Managed to get properties parsed out of the kml and into the topo json. Reduced object sizes from 50+mb in geojson to 1mb in topojson. Managed to get info windows to appear and the color of each wareda to be dependent on a specific property.

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,71 @@
 /**
-* Maps Javascript
+* Map Rendering and Interface Javascript
 */
 var infoWindow;
+var geoJsonObjects;
+var infowindow;
 
-function infoWindowContent(name, description) {
-   content =   '<div class="FT_infowindow"><h3>' + name + 
-               '</h3><div>' + description + '</div></div>';
-   return content;
-}
+// Loading of the wareda map
+$(document).ready(function(){
+  // Build the google maps base
+  var myLatlng = new google.maps.LatLng(9.195761, 40.498867);
+  var mapOptions = {
+    zoom: 6,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    scaleControl: true,
+    backgroundColor: 'transparent'
+  };
+  map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+  map.setTilt(0);
 
-//Initialization
-var myLatlng = new google.maps.LatLng(9.195761, 40.498867);
-var mapOptions = {
-  zoom: 6,
-  center: myLatlng,
-  mapTypeId: google.maps.MapTypeId.HYBRID,
-  scaleControl: true
+  // TopoJson rendering
+  $.getJSON("../static/maps/ethiopia_waredas_full_topo.json", function(data){
+        geoJsonObjects = topojson.feature(data, data.objects.ethiopia_waredas_full_converted_min)
+        map.data.addGeoJson(geoJsonObjects); 
+  }); 
 
-};
-var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-map.setTilt(0);
+  // Color each letter gray. Change the color when the isColorful property
+  // is set to true.
+  map.data.setStyle(function(feature) {
+    // Determine the color needed
+    var area = feature.getProperty('Area_km2');
+    var color = area > 1000.0 ? 'blue' : 'red';
+    return {
+      fillColor: color,
+      strokeColor: color,
+      strokeWeight: 1
+    };
+  });
+  // Set up the info window
+  infowindow = new google.maps.InfoWindow({});
+  // When a wareda is clicked, display pertinent information.
+  map.data.addListener('click', function(event) {
+    // event.feature.setProperty('isColorful', true);
+    var name = event.feature.getProperty('name');
+    infowindow.setContent(name);
+    infowindow.setPosition(event.latLng);
+    infowindow.open(map);
+  });
 
-//Country overlay
-var layer = new google.maps.FusionTablesLayer({
-  query: {
-    select: 'kml_4326', 
-    from: '420419', 
-    where: "'name_0' = 'Ethiopia'"
-  },
-  map: map,
-  suppressInfoWindows: true
+  // When the user hovers, tempt them to click by outlining the letters.
+  // Call revertStyle() to remove all overrides. This will use the style rules
+  // defined in the function passed to setStyle()
+  map.data.addListener('mouseover', function(event) {
+    map.data.revertStyle();
+    map.data.overrideStyle(event.feature, {
+      fillOpacity: 1
+      // strokeWeight: 8
+    });
+  });
+
+  map.data.addListener('mouseout', function(event) {
+    map.data.revertStyle();
+  });
 });
 
-infoWindow = new google.maps.InfoWindow();
-google.maps.event.addListener(layer, "click", function(event) {
-  infoWindow.close();
-  infoWindow.setContent(infoWindowContent(event.row.name_1.value,event.row.name_0.value));
-  infoWindow.setPosition(event.latLng);
-  infoWindow.open(map);
-});
+//Experimental KML rendering
+// var waredaLayer = new google.maps.KmlLayer({
+//   url: 'https://www.dropbox.com/s/ptq675yo6f2jrrp/ethiopia_waredas.kml'
+// });
+// waredaLayer.setMap(map);
